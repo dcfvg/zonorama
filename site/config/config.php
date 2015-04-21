bab0e197428c6048c87011db38d27b6973b82b11
dcfvg <qwsx@dcfvg.com>
2015-04-21 18:49:53 +0200

    logout url

--- site/config/config.php
+++ site/config/config.php
@@ -28,3 +28,13 @@ make Kirby work. For more fine-grained configuration
 of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
+
+c::set('routes', array(
+  array(
+    'pattern' => 'logout',
+    'action'  => function() {
+      if($user = site()->user()) $user->logout();
+      go('/');
+    }
+  )
+));
